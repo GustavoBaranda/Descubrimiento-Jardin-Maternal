755b24998881a2b4e01bb48c7f329a71b3c04108
Gustavo Baranda <baranda.gustavo@gmail.com>
2022-09-27 23:18:18 -0300

    NavBarScroll

---
+++ js/navBarScroll.js
@@ -0,0 +1,21 @@
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav ul li a');
+
+window.onscroll = () =>{
+
+    section.forEach(sec =>{
+
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links =>{
+                links.classList.remove('actives');
+                document.querySelector('header nav ul li a[href*=' + id + ']').classList.add('actives');
+            });
+        }
+
+    });
+};
