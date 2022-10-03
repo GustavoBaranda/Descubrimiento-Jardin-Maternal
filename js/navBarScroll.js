d0daba1f6f6b373648ed10529f86a51eb791ae37
Gustavo Baranda <baranda.gustavo@gmail.com>
2022-10-03 00:37:46 -0300

    correccion responsive

--- js/navBarScroll.js
+++ js/navBarScroll.js
@@ -6,7 +6,7 @@ window.onscroll = () =>{
     section.forEach(sec =>{
 
         let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
+        let offset = sec.offsetTop;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
